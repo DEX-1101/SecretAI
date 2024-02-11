681091f1e82bc3cd0d74efa15168fe048de029e4
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-02-12 02:30:50 +0700

    Update deps.py

--- deps.py
+++ deps.py
@@ -1,10 +1,9 @@
 import subprocess
 
-# ANSI color escape codes
-class Color:
-    GREEN = '\033[92m'
-    RED = '\033[91m'
-    RESET = '\033[0m'
+# ANSI escape codes for text color
+COLOR_RED = "\033[91m"
+COLOR_GREEN = "\033[92m"
+COLOR_RESET = "\033[0m"
 
 def execute_command_silently(command, message, color):
     """
@@ -13,17 +12,17 @@ def execute_command_silently(command, message, color):
     try:
         # Run the command in the subprocess silently
         subprocess.run(command, check=True, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        print(f"{color}{message} '{command}' installed successfully.{Color.RESET}")
+        print(f"{color}{message} '{command}' installed successfully.{COLOR_RESET}")
     except subprocess.CalledProcessError as e:
-        print(f"{Color.RED}Error installing '{command}': {e}{Color.RESET}")
+        print(f"{COLOR_RED}Error installing '{command}': {e}{COLOR_RESET}")
 
 if __name__ == "__main__":
     # List of commands with custom messages and colors
     commands = [
-        ("apt-get install lz4", "LZ4 package", Color.GREEN),
-        ("pip install -q colorama", "Colorama package", Color.GREEN),
-        ("curl -s -OL https://github.com/DEX-1101/sd-webui-notebook/raw/main/res/new_tunnel", "New Tunnel", Color.GREEN),
-        ("!tar -xzf zrok_0.4.23_linux_amd64.tar.gz", "Zrok package", Color.GREEN)
+        ("apt-get install lz4", "LZ4 package", COLOR_GREEN),
+        ("pip install -q colorama", "Colorama package", COLOR_RED),
+        ("curl -s -OL https://github.com/DEX-1101/sd-webui-notebook/raw/main/res/new_tunnel", "New Tunnel", COLOR_GREEN),
+        ("!tar -xzf zrok_0.4.23_linux_amd64.tar.gz", "Zrok package", COLOR_RED)
     ]
     
     # Execute each command silently with custom messages and colors
