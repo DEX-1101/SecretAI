7bb5b6959b049a1b910ec4c31a34f12fe0a07817
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-02-13 02:33:04 +0700

    Update x1101.py

--- x1101.py
+++ x1101.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     
     if is_colab:
         commands.append(("pip install -q xformers==0.0.22.post7", "xformers"))
-        else
+    else
         commands.append(("pip install -q xformers==0.0.20 triton==2.0.0", "xformers", "xformers"))
         commands.append(("pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118", "torch"))
     
