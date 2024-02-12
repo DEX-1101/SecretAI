c87015c222919c6413eb0e9333b147e77456dbe9
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-02-12 15:24:19 +0700

    Update deps.py

--- deps.py
+++ deps.py
@@ -7,6 +7,13 @@ COLOR_RED = "\033[91m"
 COLOR_GREEN = "\033[92m"
 COLOR_RESET = "\033[0m"
 
+def install_req():
+    if root_path == "/kaggle/working":
+        !pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118 -U
+        !pip install -q xformers==0.0.20 triton==2.0.0 -U 
+    else:
+        !pip install -q xformers==0.0.22.post7
+
 def kontolondon(command, message, success_count, failure_count):
     """
     Function to execute a shell command silently.
@@ -16,10 +23,10 @@ def kontolondon(command, message, success_count, failure_count):
     try:
         # Run the command in the subprocess silently
         subprocess.run(command, check=True, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        cprint(f"{message}", color="flat_cyan" + "installed.")
+        cprint(f"[ {message} ] installed", color="flat_cyan")
         success_count += 1
     except subprocess.CalledProcessError as e:
-        print(f"Error installing '{message}': {e}")
+        print(f"Error installing [ {message} ]: {e}")
         failure_count += 1
     end_time = time.time()
     return success_count, failure_count, end_time - start_time
@@ -31,8 +38,10 @@ if __name__ == "__main__":
         ("apt -y install -qq aria2", "aria2"),
         ("apt-get install lz4", "lz4"),
         ("pip install -q colorama", "colorama"),
+        ("npm install -g localtunnel", "localtunnel"),
         ("curl -s -OL https://github.com/DEX-1101/sd-webui-notebook/raw/main/res/new_tunnel", "new_tunnel"),
-        ("tar -xzf zrok_0.4.23_linux_amd64.tar.gz", "zrok")
+        ("curl -s -Lo /usr/bin/cl https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 && chmod +x /usr/bin/cl", "cloudflare"),
+        ("install_req()", "torch/xformers")
     ]
     
     # Initialize counters and total time
