4ef6e82fe59caec671a48cb6afc7013c0e2c7d0d
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-02-12 15:07:25 +0700

    Update deps.py

--- deps.py
+++ deps.py
@@ -6,7 +6,7 @@ COLOR_RED = "\033[91m"
 COLOR_GREEN = "\033[92m"
 COLOR_RESET = "\033[0m"
 
-def execute_command_silently(command, message, success_count, failure_count):
+def kontolondon(command, message, success_count, failure_count):
     """
     Function to execute a shell command silently.
     """
@@ -15,10 +15,10 @@ def execute_command_silently(command, message, success_count, failure_count):
     try:
         # Run the command in the subprocess silently
         subprocess.run(command, check=True, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        print(f"{message} installed successfully.")
+        print(f"{message} installed.")
         success_count += 1
     except subprocess.CalledProcessError as e:
-        print(f"Error installing '{command}': {e}")
+        print(f"Error installing '{message}': {e}")
         failure_count += 1
     end_time = time.time()
     return success_count, failure_count, end_time - start_time
@@ -26,10 +26,10 @@ def execute_command_silently(command, message, success_count, failure_count):
 if __name__ == "__main__":
     # List of commands with custom messages
     commands = [
-        ("apt-get install lz4", "LZ4 package"),
-        ("pip install -q colorama", "Colorama package"),
-        ("curl -s -OL https://github.com/DEX-1101/sd-webui-notebook/raw/main/res/new_tunnel", "New Tunnel"),
-        ("tar -xzf zrok_0.4.23_linux_amd64.tar.gz", "Zrok package")
+        ("apt-get install lz4", "lz4"),
+        ("pip install -q colorama", "colorama"),
+        ("curl -s -OL https://github.com/DEX-1101/sd-webui-notebook/raw/main/res/new_tunnel", "new_tunnel"),
+        ("tar -xzf zrok_0.4.23_linux_amd64.tar.gz", "zrok")
     ]
     
     # Initialize counters and total time
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     
     # Execute each command silently with custom messages
     for command, message in commands:
-        success_count, failure_count, command_time = execute_command_silently(command, message, success_count, failure_count)
+        success_count, failure_count, command_time = kontolondon(command, message, success_count, failure_count)
         total_time += command_time
     
     # Print summary
