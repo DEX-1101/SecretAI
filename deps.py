b36167e860081a344785cf1221c21e5d9f0a75dc
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-02-12 15:25:53 +0700

    Update deps.py

--- deps.py
+++ deps.py
@@ -9,10 +9,10 @@ COLOR_RESET = "\033[0m"
 
 def install_req():
     if root_path == "/kaggle/working":
-        !pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118 -U
-        !pip install -q xformers==0.0.20 triton==2.0.0 -U 
+        pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118 -U
+        pip install -q xformers==0.0.20 triton==2.0.0 -U 
     else:
-        !pip install -q xformers==0.0.22.post7
+        pip install -q xformers==0.0.22.post7
 
 def kontolondon(command, message, success_count, failure_count):
     """
