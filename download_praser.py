5bb70c6a7490d1efe8b8668408c3fba26127e993
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-05-18 16:09:11 +0700

    Update download_praser.py

--- download_praser.py
+++ download_praser.py
@@ -250,7 +250,8 @@ if __name__ == "__main__":
     si_kontol = 0
     kntl = 0
     total_time = 0
-    
+
+    print_line(0)
     cprint(f"[+] Installing Requirements", color="flat_yellow")
     for oppai, asu in rudi + yanto + agus:
         si_kontol, kntl, command_time = kontolondon(oppai, asu, si_kontol, kntl)
