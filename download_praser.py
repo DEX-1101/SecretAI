badb209e3633445d4ef35f54f5193b8fa3051384
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-05-19 16:48:50 +0700

    Update download_praser.py

--- download_praser.py
+++ download_praser.py
@@ -73,21 +73,45 @@ branch = "master"
 ui_path = os.path.join(ui, "x1101")
 git_path = os.path.join(ui_path, "extensions")
 
-def kontolondon(oppai, asu, si_kontol, kntl, debug=True):   
-    start_time = time.time() 
-    cprint(f"    > {asu}", color="flat_cyan")
-    if not os.path.exists("x1101"):
-        try:
-            if args.debug:
-                subprocess.run(oppai, check=True, shell=True, text=True)  # for debug
-            else:
-                subprocess.run(oppai, check=True, shell=True, text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            si_kontol += 1
-        except subprocess.CalledProcessError as e:
-            print(f"Error at [{asu}]: {e}")
-            kntl += 1
-    end_time = time.time()
-    return si_kontol, kntl, end_time - start_time
+import subprocess
+ui = "/kaggle/working"
+
+def run_subprocesses(commands, show_output=False):
+    processes = []
+    for i, (command, message) in enumerate(commands):
+        print(f"{message}")
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        processes.append((i, process))
+        process.wait()  # Wait for the process to complete
+        stdout, stderr = process.communicate()
+        output = stdout.decode() + stderr.decode()
+        if show_output:
+            print(output)  # Show all output for each process
+        if process.returncode != 0:
+            print(f"Subprocess {i+1} failed with error: {stderr.decode().strip()}")
+
+print_line(0)
+cprint(f"[+] Installing Requirements", color="flat_yellow")
+
+commands = [
+    ("apt-get install -y aria2", "> Installing aria2"),
+    ("npm install -g localtunnel", "> localtunnel"),
+    ("apt-get install lz4", "lz4"),
+    ("curl -s -Lo /usr/bin/cl https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 && chmod +x /usr/bin/cl", "cloudflared"),
+    (f"curl -sLO https://github.com/openziti/zrok/releases/download/v0.4.23/zrok_0.4.23_linux_amd64.tar.gz && tar -xzf zrok_0.4.23_linux_amd64.tar.gz && rm -rf zrok_0.4.23_linux_amd64.tar.gz && mv {ui}/zrok /usr/bin", "zrok"),
+    (f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/x1101/UI/resolve/main/ui.tar.lz4 -o ui.tar.lz4 && tar -xI lz4 -f ui.tar.lz4 && mv -f {ui}/kaggle/working/x1101 {ui} && rm {ui}/ui.tar.lz4 && rm -rf {ui}/kaggle", "Installing UI..."),
+    (f"cd {ui_path} && git reset --hard && git pull && git switch {branch} && git pull && git reset --hard", "Updating UI..."),
+    (f"rm -rf {git_path}/* && cd {git_path} && git clone https://github.com/BlafKing/sd-civitai-browser-plus && git clone https://github.com/Mikubill/sd-webui-controlnet && git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete && git clone https://github.com/DEX-1101/sd-encrypt-image && git clone https://github.com/DEX-1101/timer && git clone https://github.com/gutris1/sd-hub && git clone https://github.com/Bing-su/adetailer.git && git clone https://github.com/zanllp/sd-webui-infinite-image-browsing && git clone https://github.com/thomasasfk/sd-webui-aspect-ratio-helper && git clone https://github.com/hako-mikan/sd-webui-regional-prompter && git clone https://github.com/picobyte/stable-diffusion-webui-wd14-tagger && git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111 && git clone https://github.com/Haoming02/sd-webui-tabs-extension", "Cloning Extensions..."),
+    ("echo 'Process 3'", "Running Process 3")
+]
+
+if 'content' in os.listdir('/'):
+    agus.append(("pip install xformers==0.0.25 --no-deps", "Installing xformers..."))
+    agus.append(("", "Done"))
+elif 'kaggle' in os.listdir('/'):
+    agus.append(("pip install torch==2.1.2+cu121 torchvision==0.16.2+cu121 torchaudio==2.1.2 --extra-index-url https://download.pytorch.org/whl/cu121", "Installing torch..."))
+    agus.append(("pip install xformers==0.0.23.post1 triton==2.1.0", "Installing xformers..."))
+    agus.append(("", "Done"))
 
 ################# UI ##################
 
@@ -282,47 +306,9 @@ if __name__ == "__main__":
     download_from_link_file(link_file_path)
 
     ############### UI ####################  
-    rudi = [
-        ("apt-get update", "Updating library..."),
-        ("apt-get install lz4", "lz4"),
-        ("npm install -g localtunnel", "localtunnel"),
-        ("curl -s -Lo /usr/bin/cl https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 && chmod +x /usr/bin/cl", "cloudflared"),
-        (f"curl -sLO https://github.com/openziti/zrok/releases/download/v0.4.23/zrok_0.4.23_linux_amd64.tar.gz && tar -xzf zrok_0.4.23_linux_amd64.tar.gz && rm -rf zrok_0.4.23_linux_amd64.tar.gz && mv {ui}/zrok /usr/bin", "zrok")
-    ]
-
-    yanto = [
-        (f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/x1101/UI/resolve/main/ui.tar.lz4 -o ui.tar.lz4 && tar -xI lz4 -f ui.tar.lz4 && mv -f {ui}/kaggle/working/x1101 {ui} && rm {ui}/ui.tar.lz4 && rm -rf {ui}/kaggle", "Installing UI..."),
-        (f"cd {ui_path} && git reset --hard && git pull && git switch {branch} && git pull && git reset --hard", "Updating UI..."),
-        (f"rm -rf {git_path}/* && cd {git_path} && git clone https://github.com/BlafKing/sd-civitai-browser-plus && git clone https://github.com/Mikubill/sd-webui-controlnet && git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete && git clone https://github.com/DEX-1101/sd-encrypt-image && git clone https://github.com/DEX-1101/timer && git clone https://github.com/gutris1/sd-hub && git clone https://github.com/Bing-su/adetailer.git && git clone https://github.com/zanllp/sd-webui-infinite-image-browsing && git clone https://github.com/thomasasfk/sd-webui-aspect-ratio-helper && git clone https://github.com/hako-mikan/sd-webui-regional-prompter && git clone https://github.com/picobyte/stable-diffusion-webui-wd14-tagger && git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111 && git clone https://github.com/Haoming02/sd-webui-tabs-extension", "Cloning Extensions...")
-    ]
-
-    agus = []
+   
+    run_subprocesses(commands)
     
-    if 'content' in os.listdir('/'):
-        agus.append(("pip install xformers==0.0.25 --no-deps", "Installing xformers..."))
-        agus.append(("", "Done"))
-    elif 'kaggle' in os.listdir('/'):
-        agus.append(("pip install torch==2.1.2+cu121 torchvision==0.16.2+cu121 torchaudio==2.1.2 --extra-index-url https://download.pytorch.org/whl/cu121", "Installing torch..."))
-        agus.append(("pip install xformers==0.0.23.post1 triton==2.1.0", "Installing xformers..."))
-        agus.append(("", "Done"))
-    else:
-        agus.append((""))
-    
-################
-    
-    si_kontol = 0
-    kntl = 0
-    total_time = 0
-
-    print_line(0)
-    cprint(f"[+] Installing Requirements", color="flat_yellow")
-    for oppai, asu in rudi + yanto + agus:
-        si_kontol, kntl, command_time = kontolondon(oppai, asu, si_kontol, kntl)
-        total_time += command_time
-        
-    #print_line(0)
-    #cprint(f"[+] {kntl} of {si_kontol} error found. All completed within: {total_time:.2f} secs", color="flat_yellow")
-
     if args.config:
         subprocess.run(f"wget -q {import_config} -O {ui}/config.json", shell=True)
     
